cbb05ad52010f6aedb622d177a98e293775fd65a
vignesh1809-stack <balusuvignesh@gmail.com>
2024-09-25 13:56:41 +0530

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,25 @@
+import React,{ useState } from 'react'
+import Create from './Create'
+
+function Home(){
+        const [todos, setTodos] = useState([])
+    return(
+        <div className='home'>
+            <h2>Todo List</h2>
+            <Create/>
+            {
+                todos.length ===0 ?
+                <div><h2>No Record</h2></div>
+                :
+                todos.map(todo =>(
+                    <div>
+                        {todo}
+                    </div>
+                ))
+            }
+
+        </div>
+    )
+}
+
+export default Home
